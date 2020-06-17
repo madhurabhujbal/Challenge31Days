6ac3c88cf78f50807f85bcb31f38ea6a0721371f
Madhura <madhura.bhujbal@gmail.com>
2020-06-17 16:16:16 +0100

    Add js code to js file

--- day1/clock.js
+++ day1/clock.js
@@ -1,21 +1,23 @@
 
-let hand_hr = document.querySelector('.hand.hour');
-let hand_min = document.querySelector('.hand.minute');
-let hand_sec = document.querySelector('.hand.second');
+const hand_hr = document.querySelector('.hand.hour');
+const hand_min = document.querySelector('.hand.minute');
+const hand_sec = document.querySelector('.hand.second');
 
 function tick() {
-    if(hand_sec == null) {
-        hand_hr = document.querySelector('.hand.hour');
-        hand_min = document.querySelector('.hand.minute');
-        hand_sec = document.querySelector('.hand.second');
-        return;
-    }
     const d = new Date();
     //Seconds
     let seconds = d.getSeconds();
-    const secInDegrees = ((seconds / 60) * 360) + 90;
+    const secInDegrees = (seconds * (360 / 60)) + 90;
     hand_sec.style.transform = `translateY(-50%) rotate(${secInDegrees}deg)`;
+    //Minutes
+    let minutes = d.getMinutes();
+    const minInDegrees = (minutes * (360 / 60)) + 90;
+    hand_min.style.transform = `translateY(-50%) rotate(${minInDegrees}deg)`;
+    //Hours
+    let hours = d.getHours();
+    const hoursInDegrees = (hours * (360 / 12)) + (minutes * (30 / 60)) + 90;
+    hand_hr.style.transform = `translateY(-50%) rotate(${hoursInDegrees}deg)`;
 }
-
-tick();
-setInterval(tick, 1000);
+//For ticking the clock, uncomment the two lines below and watch in browser
+// tick();
+// setInterval(tick, 1000);
