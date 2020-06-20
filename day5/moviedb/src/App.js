7a0d8a4cdda8cfd426a55e670a3af07d363c0a67
Madhura <madhura.bhujbal@gmail.com>
2020-06-20 18:22:55 +0100

    Write function for handling input and check if movie typed is being stored in variable

--- day5/moviedb/src/App.js
+++ day5/moviedb/src/App.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Search from './components/Search';
 
 function App() {
+  const [state, setState] = useState({
+    s: "", //search query
+    results: [],
+    selected: {} //Movie we select
+  });
+
   // OMDb API: http://www.omdbapi.com/?i=tt3896198&apikey=e0a0442a
   const apiUrl = "http://www.omdbapi.com/?apikey=e0a0442a";
+
+  const handleInput = (e) => {
+    let s = e.target.value;
+    setState(prevState => {
+      return { ...prevState, s : s }
+    });
+    console.log(state.s);
+  }
+
   return (
     <div className="App">
       <header>
         <h1> Movie Database </h1>
       </header>
       <main>
-        <Search />
+        <Search handleInput={handleInput} />
       </main>
     </div>
   );
