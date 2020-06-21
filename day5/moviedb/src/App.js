473af5404a5257ff2197d3531055803caf03e11c
Madhura <madhura.bhujbal@gmail.com>
2020-06-21 12:41:04 +0100

    Import popup component and add code for handling popup

--- day5/moviedb/src/App.js
+++ day5/moviedb/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Search from './components/Search';
 import Results from './components/Results';
+import Popup from './components/Popup';
 import axios from 'axios';
 
 function App() {
@@ -59,8 +60,12 @@ function App() {
         <h1> Movie Database </h1>
       </header>
       <main>
+
         <Search handleInput={handleInput} search={search} />
-        <Results results={state.results} />
+        <Results results={state.results} openPopup={openPopup}/>
+
+        {(typeof state.selected.Title != "undefined") ? <Popup selected={state.selected} closePopup={closePopup} /> : false}
+
       </main>
     </div>
   );
