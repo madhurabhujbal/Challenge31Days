03432c17b3028a9de0d807da260961d693fdd7a7
Madhura <madhura.bhujbal@gmail.com>
2020-06-22 11:54:54 +0100

    Add code for displaying header and searchbox on screen and style it

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
 import axios from 'axios';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
   const apiUrl = "http://www.omdbapi.com/?apikey=e0a0442a";
@@ -12,7 +12,11 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}> Movie DB </Text>
+      <TextInput
+        style={styles.searchbox}
+        value={state.s}
+      />
     </View>
   );
 }
@@ -20,8 +24,28 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#223343',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    paddingTop: 70,
+    paddingHorizontal: 20
   },
+
+  title: {
+    color: '#FFF',
+    fontSize: 32,
+    fontWeight: '700',
+    textAlign: 'center',
+    marginBottom: 20
+  },
+
+  searchbox: {
+    fontSize: 20,
+    fontWeight: '300',
+    padding: 20,
+    width: '100%',
+    backgroundColor: '#FFF',
+    borderRadius: 8,
+    marginBottom: 40
+  }
 });
