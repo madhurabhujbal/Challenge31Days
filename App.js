9bb48c98ed8df4e739692b27bfa62956089aab06
Madhura <madhura.bhujbal@gmail.com>
2020-06-22 11:26:42 +0100

    Include api key for omdb app

--- App.js
+++ App.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import axios from 'axios';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const apiUrl = "http://www.omdbapi.com/?apikey=e0a0442a";
+  const [state, setState] = useState({
+    s: "Enter a movie...",
+    results: [],
+    selected: {}
+  });
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
