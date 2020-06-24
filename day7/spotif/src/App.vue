93286e1eaeceb9c7b40dba19f1c97d8af0f6e141
Madhura <madhura.bhujbal@gmail.com>
2020-06-24 16:54:44 +0100

    Add controls button and set play or pause state according to current state of song

--- day7/spotif/src/App.vue
+++ day7/spotif/src/App.vue
@@ -6,6 +6,12 @@
     <main>
       <section class="player">
         <h2 class="song-title"> {{ current.title }} - <span>{{ current.artist }}</span> </h2>
+        <div class="controls">
+          <button class="prev">Prev</button>
+          <button class="play" v-if="!isPlaying">Play</button>
+          <button class="pause" v-else>Pause</button>
+          <button class="next">Next</button>
+        </div>
       </section>
     </main>
   </div>
@@ -19,6 +25,7 @@ export default {
     return {
       current: {},
       index : 0,
+      isPlaying: false,
       songs: [
         {
           title: 'Broke for free',
@@ -59,7 +66,6 @@ export default {
   created() {
     this.current = this.songs[this.index];
     this.player.src = this.current.src;
-    this.player.play();
   }
 }
 </script>
