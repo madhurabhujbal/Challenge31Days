f2631f6202b43f1e9a1b6d8200f508921ae8363d
Madhura <madhura.bhujbal@gmail.com>
2020-06-25 15:21:35 +0100

    Keep only 3 product in the product list and delete others

--- day8_productCards/product_cards/src/App.vue
+++ day8_productCards/product_cards/src/App.vue
@@ -21,18 +21,6 @@ export default {
     return {
       products: [
         {
-          title: 'Nike Black Shoes',
-          color: 'black',
-          bgtext: 'NIKE',
-          src: require('./assets/blackShoes.jpg')
-        },
-        {
-          title: 'Nike Fluorescent Shoes',
-          color: 'fluorescent green',
-          bgtext: 'AIR',
-          src: require('./assets/fluorescentShoes.jpg')
-        },
-        {
           title: 'Nike Green Shoes',
           color: 'green',
           bgtext: 'MAX',
@@ -49,24 +37,6 @@ export default {
           color: 'grey, orange, blue',
           bgtext: 'LIGHT',
           src: require('./assets/multicolorShoes.jpg')
-        },
-        {
-          title: 'Nike Black Shoes',
-          color: 'black, red',
-          bgtext: 'SPORTS',
-          src: require('./assets/netRedShoes.jpg')
-        },
-        {
-          title: 'Nike Black Shoes',
-          color: 'red',
-          bgtext: 'RUN',
-          src: require('./assets/redShoes.jpg')
-        },
-        {
-          title: 'Nike Tricolor Shoes',
-          color: 'black, green, grey',
-          bgtext: 'JUST DO IT',
-          src: require('./assets/tricolorNetShoes.jpg')
         },
       ]
     }
