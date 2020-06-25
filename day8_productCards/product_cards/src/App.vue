206b8d9f1e58983e2c36a142897e89828ecb2879
Madhura <madhura.bhujbal@gmail.com>
2020-06-25 23:31:26 +0100

    Add code to move background text and also move product in z-coordinate

--- day8_productCards/product_cards/src/App.vue
+++ day8_productCards/product_cards/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <main id="app">
+  <main id="app" @mousemove="mousemove">
     <section class="products">
       <Product
         v-for="product in products"
@@ -39,6 +39,35 @@ export default {
           src: require('./assets/pinkShoe.png')
         },
       ]
+    }
+  },
+  methods: {
+    mousemove (e) {
+      let mouseX = e.clientX;
+      let mouseY = e.clientY;
+
+      let products = document.querySelectorAll('.products .product');
+
+      for(let i = 0; i < products.length; i++) {
+        let product = products[i];
+        let product_image = product.querySelector('.product-image-wrap');
+
+        let img_x = mouseX - this.coords(product_image).x;
+        let img_y = mouseY - this.coords(product_image).y;
+        product_image.style.transform = `translateY(-${img_y/30}px) translateX(-${img_x/30}px) translateZ(100px)`;
+
+        let bgtext = product.querySelector('.bg-text');
+        let bg_x = mouseX - this.coords(bgtext).x;
+        let bg_y = mouseY - this.coords(bgtext).y;
+        bgtext.style.transform = `translateX(${bg_x/25}px) translateY(${bg_y/25}px)`;
+      }
+    },
+    coords (el) {
+      let coords = el.getBoundingClientRect();
+      return {
+        x: coords.left / 2,
+        y: coords.top / 2,
+      }
     }
   }
 }
