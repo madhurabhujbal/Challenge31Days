8efc1cbd094aeca0f0fc340e83c43983ec37aec3
Madhura <madhura.bhujbal@gmail.com>
2020-06-25 14:38:17 +0100

    Add html code to map products from product list and  display product title on local host

--- day8_productCards/product_cards/src/components/Product.vue
+++ day8_productCards/product_cards/src/components/Product.vue
@@ -1,5 +1,12 @@
 <template>
     <div class="product">
-        Product
+        {{ product.title }}
     </div>
 </template>
+
+<script>
+export default {
+    name: 'product',
+    props: ['product']
+}
+</script>
