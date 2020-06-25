b69c66dd0399e9b3fac2ac0c3eff7d1372435285
Madhura <madhura.bhujbal@gmail.com>
2020-06-25 14:52:46 +0100

    Add code to display all products and their details to max 20 words

--- day8_productCards/product_cards/src/components/Product.vue
+++ day8_productCards/product_cards/src/components/Product.vue
@@ -1,6 +1,17 @@
 <template>
     <div class="product">
-        {{ product.title }}
+        <div class="product-inner">
+            <div class="product-text-wrap">
+                <h2 class="bg-text"> {{product.bgtext}} </h2>
+            </div>
+            <div class="product-image-wrap">
+                <img :src="product.src" class="image" />
+            </div>
+            <div class="product-detail">
+                <h2> {{product.title}} </h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta modi iusto sint! Sed natus fuga, sequi pariatur at nisi modi!</p>
+            </div>
+        </div>
     </div>
 </template>
 
