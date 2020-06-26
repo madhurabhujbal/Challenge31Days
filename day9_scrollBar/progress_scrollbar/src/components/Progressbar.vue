b184c1aa93da06ee2bd6d7670eafe7b8190c9eb5
Madhura <madhura.bhujbal@gmail.com>
2020-06-26 15:49:58 +0100

    Create a new component for progress bar and add html, js and styling code for the same

---
+++ day9_scrollBar/progress_scrollbar/src/components/Progressbar.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="progress-bar">
+        <div class="progress-inner">
+            <span>0%</span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'progressbar'
+}
+</script>
+
+<style>
+    .progress-bar {
+        width: 100%;
+        height: 20px;
+        background-color: #FFF;
+        position: relative;
+    }
+
+    .progress-inner {
+        height: 100%;
+        background-image: linear-gradient(to right, #ED008D, #2484E4);
+        border-radius: 0px 10px 10px 0px;
+    }
+
+    .progress-inner span {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        color: #313131;
+        font-size: 14px;
+    }
+
+</style>
