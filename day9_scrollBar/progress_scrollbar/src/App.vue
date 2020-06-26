d96cc995d2d2391b357657b5f5e2a61c34b54bd4
Madhura <madhura.bhujbal@gmail.com>
2020-06-26 12:57:06 +0100

    Add html code and styling for header and main body of screen

--- day9_scrollBar/progress_scrollbar/src/App.vue
+++ day9_scrollBar/progress_scrollbar/src/App.vue
@@ -1,5 +1,10 @@
 <template>
   <div id="app">
+    <main>
+      <header>
+        <h1> Scroll Progress Bar </h1>
+      </header>
+    </main>
   </div>
 </template>
 
@@ -12,4 +17,32 @@ export default {
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'montseratt', sans-serif;
+}
+
+main {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  overflow: hidden;
+}
+
+header {
+  background-color: #313131;
+  height: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+header h1 {
+  color: #FFF;
+  font-size: 30px;
+  text-align: center;
+}
+
 </style>
