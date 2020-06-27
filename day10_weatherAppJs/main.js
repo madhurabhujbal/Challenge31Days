d368044ccf6a1bfae7a499d8585adb5974c7cbbf
Madhura <madhura.bhujbal@gmail.com>
2020-06-27 20:35:27 +0100

    Add code to get weather data of entered city from openweather site and check on console

--- day10_weatherAppJs/main.js
+++ day10_weatherAppJs/main.js
@@ -1,6 +1,6 @@
 const api = {
     key: "446cc30bc37be1228e1d55b09335aeb6",
-    baseurl: "http://api.openweathermap.org/data/2.5/"
+    base: "http://api.openweathermap.org/data/2.5/"
 }
 
 const searchbox = document.querySelector('.search-box');
@@ -8,7 +8,18 @@ searchbox.addEventListener('keypress', setQuery);
 
 function setQuery(evt) {
     if (evt.keyCode == 13) { //13 is keycode for Enter button
-        //getResults(searchbox.value);
+        getResults(searchbox.value);
         console.log(searchbox.value);
     }
+}
+
+function getResults(query) {
+    fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+    .then(weather => {
+        return weather.json();
+    }).then(displayResults);
+}
+
+function displayResults(weather) {
+    console.log(weather);
 }
