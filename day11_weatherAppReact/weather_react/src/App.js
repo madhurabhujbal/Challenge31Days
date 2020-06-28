a00827825f38d4b5ac31f27a37831699a7134882
Madhura <madhura.bhujbal@gmail.com>
2020-06-28 10:41:34 +0100

    Add setQuery method in promise and check if weather info is retrieved on console

--- day11_weatherAppReact/weather_react/src/App.js
+++ day11_weatherAppReact/weather_react/src/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 const api = {
   key: "446cc30bc37be1228e1d55b09335aeb6",
   base: "http://api.openweathermap.org/data/2.5/"
 }
 
 function App() {
-    const dateBuilder = function (d) {
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+
+  const search = evt => {
+    if(evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+      .then(res => res.json())
+      .then(result => {
+        setWeather(result);
+        setQuery('');
+        console.log(result);
+      });
+    }
+  }
+
+  const dateBuilder = function (d) {
     let months = ["Janauary", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
@@ -24,7 +39,11 @@ function App() {
           <input
           type="text"
           className="search-bar"
-          placeholder="Search..." />
+          placeholder="Search..."
+          onChange={e => setQuery(e.target.value)}
+          value={query}
+          onKeyPress={search}
+          />
         </div>
         <div className="location-box">
           <div className="location">New York, US</div>
