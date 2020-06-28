ecf4c040577c9b46c9675cff2a18747afca9c92a
Madhura <madhura.bhujbal@gmail.com>
2020-06-28 08:08:06 +0100

    Add code for displaying searchbar on screen

--- day11_weatherAppReact/weather_react/src/App.js
+++ day11_weatherAppReact/weather_react/src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
+const api = {
+  key: "446cc30bc37be1228e1d55b09335aeb6",
+  base: "http://api.openweathermap.org/data/2.5/"
+}
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input
+          type="text"
+          className="search-bar"
+          placeholder="Search..." />
+        </div>
+      </main>
     </div>
   );
 }
