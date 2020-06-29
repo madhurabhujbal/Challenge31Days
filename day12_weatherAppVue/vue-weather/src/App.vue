02e62c34fcd791283efae83ebc8f227748ab09c4
Madhura <madhura.bhujbal@gmail.com>
2020-06-29 10:26:15 +0100

    Create new vue project and delete unnecessary code and files

---
+++ day12_weatherAppVue/vue-weather/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  // data() {
+  //   return {
+  //     api_key : '446cc30bc37be1228e1d55b09335aeb6'
+  //   }
+  // }
+}
+</script>
+
+<style>
+</style>
