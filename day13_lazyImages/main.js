a45e08470fca3efe3c9958ff5b990f43cfda2dd2
Madhura <madhura.bhujbal@gmail.com>
2020-06-30 15:56:42 +0100

    Add code to load the image and changing image source as we scroll down

--- day13_lazyImages/main.js
+++ day13_lazyImages/main.js
@@ -9,7 +9,14 @@ const observer = new IntersectionObserver(imageObserver, options);
 function imageObserver(entries, observer) {
     //entries are all of the observed items
     entries.forEach(entry => {
-        console.log(entry);
+        //console.log(entry);
+        if (entry.isIntersecting) {
+            //We first take the image and change its source where we want to lazy load the image
+            const img = entry.target;
+            const img_src = img.dataset.src;//taking data from data-src in html
+            console.log("Lazy loading", img);
+            img.src = img_src;
+        }
     })
 }
 
