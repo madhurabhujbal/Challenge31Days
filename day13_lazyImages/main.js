ef7852596edceb1ca45ebff0680ba5e4b5bb1704
Madhura <madhura.bhujbal@gmail.com>
2020-07-01 10:14:57 +0100

    Add customization for practice

--- day13_lazyImages/main.js
+++ day13_lazyImages/main.js
@@ -1,24 +1,33 @@
 
-let options = {
-    threshold: 1 //Indicates how far image should be relative to bottom of page before it loads. with value=1 means loaded image should be fully on screen/in view before next image is loaded
+// let options = {
+//     threshold: 1 //Indicates how far image should be relative to bottom of page before it loads. with value=1 means loaded image should be fully on screen/in view before next image is loaded
+
+// }
+
+const observer = new IntersectionObserver(imageObserver, {threshold: 0.5});
+
+function replaceOriginalImage(entry, observer) {
+    // if entry is intersecting/visible enough
+    if(entry.isIntersecting) {
+
+        // first retrieve the image tag/DOM
+        console.log("Entry: ", entry);
+        let img = entry.target;
+        console.log("Image: ", img);
+        // retrieve the replacement/actual image
+        // and replace the placeholder image with replacement/actual image
+        img.src = img.dataset.src;
+        observer.unobserve(img);
+    }
 
 }
 
-const observer = new IntersectionObserver(imageObserver, options);
-
 function imageObserver(entries, observer) {
     //entries are all of the observed items
-    entries.forEach(entry => {
-        //console.log(entry);
-        if (entry.isIntersecting) {
-            //We first take the image and change its source where we want to lazy load the image
-            const img = entry.target;
-            const img_src = img.dataset.src;//taking data from data-src in html
-            console.log("Lazy loading", img);
-            img.src = img_src;
-            observer.unobserve(img);
-        }
-    })
+
+    for(let i = 0; i < entries.length; i++) {
+        replaceOriginalImage(entries[i], observer);
+    }
 }
 
 //Access all images
