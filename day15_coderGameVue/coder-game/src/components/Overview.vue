0ea8779c07be3aff6ad3c4d44f88773a535cbfa2
Madhura <madhura.bhujbal@gmail.com>
2020-07-02 12:06:32 +0100

    Create a new file and add markup and methods for displaying and calculating values of game fields dynamically

---
+++ day15_coderGameVue/coder-game/src/components/Overview.vue
@@ -0,0 +1,31 @@
+<!-- This file will hold current values of fields on app -->
+
+<template>
+    <div class="overview">
+        <h3>Current Bytes</h3>
+        <div class="bytes">{{ bytes }}</div>
+        <div class="bps">Bytes per second: {{ bps }}</div>
+        <div class="level">Current Level: {{ level }}</div>
+        <div class="next-level">Bytes Until Next Level: {{bytesUntilNextLevel}}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'overview',
+    computed: { //Where all computations will take place
+        bytes() {
+            return Math.round(this.$store.state.bytes); //Accessing bytes from store
+        },
+        bps() {
+            return (this.$store.state.bps).toFixed(1);
+        },
+        level() {
+            return this.$store.state.player.level;
+        },
+        bytesUntilLevelUp() {
+            return this.$store.getters.bytesUntilLevelUp;
+        }
+    }
+}
+</script>
