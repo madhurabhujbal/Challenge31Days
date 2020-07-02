55835bced5fef5aeaf2a30e9b4066408d3678f8b
Madhura <madhura.bhujbal@gmail.com>
2020-07-02 18:17:12 +0100

    Add markup and styling code for displaying buttons on app screen. Add condition in markup code for displaying buttons

--- day15_coderGameVue/coder-game/src/components/Upgrades.vue
+++ day15_coderGameVue/coder-game/src/components/Upgrades.vue
@@ -4,7 +4,9 @@
         :key="index"
         class="upgrade"
         >
-        {{ upgrade.name }}
+        <button :class="`button ${upgrade.disabled ? 'disabled' : ''}`">
+        {{ upgrade.name }} {{ upgrade.disabled ? `(lvl: ${upgrade.unlocksAt})` : ''}}
+        </button>
         </div>
     </div>
 </template>
@@ -19,3 +21,39 @@ export default {
     }
 }
 </script>
+
+<style lang="scss">
+    .upgrades {
+        background-color: #222;
+        padding: 25px;
+
+        .upgrade {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            margin: 0px -15px 15px;
+        }
+
+        .button, .cost, .quantity {
+            color: #FFF;
+            margin: 0px 15px;
+        }
+
+        .button {
+            appearance: none;
+            border: none;
+            outline: none;
+            background: none;
+            display: inline;
+            min-width: 300px;
+            padding: 15px 25px;
+            background-color: #28A484;
+            color: #FFF;
+            font-size: 20px;
+            font-weight: 700;
+            text-align: center;
+            text-transform: uppercase;
+            cursor: pointer;
+        }
+    }
+</style>
