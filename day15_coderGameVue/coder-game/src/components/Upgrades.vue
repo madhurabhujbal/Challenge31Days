b6d865614f5c90bc1caf004ef64f670fa4838e04
Madhura <madhura.bhujbal@gmail.com>
2020-07-02 17:58:59 +0100

    Change the path of upgrades to be returned

--- day15_coderGameVue/coder-game/src/components/Upgrades.vue
+++ day15_coderGameVue/coder-game/src/components/Upgrades.vue
@@ -14,7 +14,7 @@ export default {
     name: 'upgrades',
     computed: {
         upgrades() {
-            return this.$store.state.upgrades;
+            return this.$store.getters.availableUpgrades;
         }
     }
 }
