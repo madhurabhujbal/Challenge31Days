70f1c166ab79a020e4c6340c561a64f3230dfb64
Madhura <madhura.bhujbal@gmail.com>
2020-07-03 12:07:03 +0100

    Add styling code for main page and button

--- day16_popupModalVue/popup-modal/src/App.vue
+++ day16_popupModalVue/popup-modal/src/App.vue
@@ -22,4 +22,48 @@ export default {
 </script>
 
 <style>
+  * {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: "montserrat", sans-serif;
+}
+
+#app {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  min-height: 100vh;
+  overflow-x: hidden;
+}
+
+.button {
+  appearance: none;
+  outline: none;
+  border: none;
+  background: none;
+  cursor: pointer;
+
+  display: inline-block;
+  padding: 35px 45px;
+  background-image: linear-gradient(to right, #CC2E5D, #FF5858);
+  border-radius: 8px;
+  color: #FFF;
+
+  font-size: 18px;
+  font-weight: 700;
+
+  box-shadow: 3px 3px rgba(0, 0, 0, 0.4);
+  transition: 0.4s ease-out;
+}
+
+.button:hover {
+  box-shadow: 6px 6px rgba(0, 0, 0, 0.6);
+}
+
 </style>
