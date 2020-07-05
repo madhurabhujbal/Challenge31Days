58aeba0176090e8f3e5e83c4422d14f29cbb7ff6
Madhura <madhura.bhujbal@gmail.com>
2020-07-05 16:52:20 +0100

    Create a new dropdown component and add markup and js code to it

---
+++ day17_dropdownMenuVue/dropdown-vue/src/components/Dropdown.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="menu-item">
+        <a href="#">
+            {{ title }}
+        </a>
+        <svg viewBox="0 0 1030  638">
+            <path d="M1017 68L541 626q-11 12-26 12t-26-12L13 68Q-3 49 6 24.5T39 0h952q24 0 33 24.5t-7 43.5z" fill="#FFF">
+            </path>
+        </svg>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'dropdown'
+}
+</script>
+
+<style>
+
+</style>
