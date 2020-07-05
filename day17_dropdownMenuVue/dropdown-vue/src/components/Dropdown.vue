e74244d0341377eade7799e259d64dd7d864fea8
Madhura <madhura.bhujbal@gmail.com>
2020-07-05 17:15:54 +0100

    Add props and styling to dropdown

--- day17_dropdownMenuVue/dropdown-vue/src/components/Dropdown.vue
+++ day17_dropdownMenuVue/dropdown-vue/src/components/Dropdown.vue
@@ -3,7 +3,7 @@
         <a href="#">
             {{ title }}
         </a>
-        <svg viewBox="0 0 1030  638">
+        <svg viewBox="0 0 1030  638" width="10">
             <path d="M1017 68L541 626q-11 12-26 12t-26-12L13 68Q-3 49 6 24.5T39 0h952q24 0 33 24.5t-7 43.5z" fill="#FFF">
             </path>
         </svg>
@@ -12,10 +12,14 @@
 
 <script>
 export default {
-    name: 'dropdown'
+    name: 'Dropdown',
+    props: ['title', 'items']
 }
 </script>
 
 <style>
-
+nav .menu-item svg {
+    width: 10px;
+    margin-left: 10px;
+}
 </style>
