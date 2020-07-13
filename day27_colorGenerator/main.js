f5260ec5f8d543d2f62625188346ae4319223afa
Madhura <madhura.bhujbal@gmail.com>
2020-07-13 16:34:05 +0100

    Add js code to actually copy color name to clipboard

--- day27_colorGenerator/main.js
+++ day27_colorGenerator/main.js
@@ -16,6 +16,10 @@ function copy() {
     let input = document.createElement('input');
     input.setAttribute('value', text);
     document.body.appendChild(input);
+    input.select();
+
+    document.execCommand('copy');
+    document.body.removeChild(input);
 
     alert('Copied: ' + text);
 }
