be81d9a2ad5a96fec96a1e002d3bbe92e2edbd9b
Madhura <madhura.bhujbal@gmail.com>
2020-07-15 16:16:18 +0100

    Add js code for closing menu list when close menu is clicked

--- day29_responsiveNavbar/main.js
+++ day29_responsiveNavbar/main.js
@@ -1,9 +1,10 @@
 //Toggle mobile menu
 const open_menu = document.querySelector('.open-menu');
-//const close_menu = document.querySelector('.close-menu');
+const close_menu = document.querySelector('.close-menu');
 const navbar = document.querySelector('.navbar');
 
 open_menu.addEventListener('click', toggleMenu);
+close_menu.addEventListener('click', toggleMenu);
 
 function toggleMenu() {
     navbar.classList.toggle('is-active');
