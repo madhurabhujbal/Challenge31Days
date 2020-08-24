7c5d69529cbdf5ecef07c19606de83f408e88344
Madhura <madhura.bhujbal@gmail.com>
2020-08-24 15:11:38 +0100

    Add if condition to check for result array

--- day5_moviedb_React/src/App.js
+++ day5_moviedb_React/src/App.js
@@ -1,14 +1,14 @@
-import React, { useState } from 'react';
-import Search from './components/Search';
-import Results from './components/Results';
-import Popup from './components/Popup';
-import axios from 'axios';
+import React, { useState } from "react";
+import Search from "./components/Search";
+import Results from "./components/Results";
+import Popup from "./components/Popup";
+import axios from "axios";
 
 function App() {
   const [state, setState] = useState({
     s: "", //search query
     results: [],
-    selected: {} //Movie we select
+    selected: {}, //Movie we select
   });
 
   // OMDb API: http://www.omdbapi.com/?i=tt3896198&apikey=e0a0442a
@@ -16,43 +16,46 @@ function App() {
 
   const search = (e) => {
     if (e.key === "Enter") {
-      axios(apiUrl + "&s=" + state.s)
-      .then(({data}) => {
+      axios(apiUrl + "&s=" + state.s).then(({ data }) => {
         let results = data.Search;
-        setState(prevState => {
-          return{ ...prevState, results: results }
+        console.log(results);
+        if (results === undefined) {
+          setState((prevState) => {
+            return { ...prevState, results: "error found" };
+          });
+        }
+        setState((prevState) => {
+          return { ...prevState, results: results };
         });
-
       });
     }
-  }
+  };
 
   const handleInput = (e) => {
     let s = e.target.value;
-    setState(prevState => {
-      return { ...prevState, s : s }
+    setState((prevState) => {
+      return { ...prevState, s: s };
     });
 
     //check text entered in search textbox is getting stored in variable s
     //console.log(state.s);
-  }
+  };
 
-  const openPopup = id => {
-    axios(apiUrl + "&i=" +id)
-    .then(({ data }) => {
+  const openPopup = (id) => {
+    axios(apiUrl + "&i=" + id).then(({ data }) => {
       let result = data;
 
-      setState(prevState => {
-        return{...prevState, selected: result}
+      setState((prevState) => {
+        return { ...prevState, selected: result };
       });
     });
-  }
+  };
 
   const closePopup = () => {
-    setState(prevState => {
-      return{...prevState, selected: {}}
+    setState((prevState) => {
+      return { ...prevState, selected: {} };
     });
-  }
+  };
 
   return (
     <div className="App">
@@ -60,12 +63,14 @@ function App() {
         <h1> Movie Database </h1>
       </header>
       <main>
-
         <Search handleInput={handleInput} search={search} />
-        <Results results={state.results} openPopup={openPopup}/>
+        <Results results={state.results} openPopup={openPopup} />
 
-        {(typeof state.selected.Title != "undefined") ? <Popup selected={state.selected} closePopup={closePopup} /> : false}
-
+        {typeof state.selected.Title != "undefined" ? (
+          <Popup selected={state.selected} closePopup={closePopup} />
+        ) : (
+          false
+        )}
       </main>
     </div>
   );
