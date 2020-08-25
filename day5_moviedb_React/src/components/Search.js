19b887117d6680f9bb38a73751d9f8fb63cb3e09
Madhura <madhura.bhujbal@gmail.com>
2020-08-25 17:32:48 +0100

    Remove handleInput method to enable search done only on Enter keypress

--- day5_moviedb_React/src/components/Search.js
+++ day5_moviedb_React/src/components/Search.js
@@ -1,16 +1,16 @@
-import React from 'react'
+import React from "react";
 
-function Search({handleInput, search}) {
-    return (
-        <section className="searchbox-wrap">
-            <input type="text"
-            placeholder="Search for a movie..."
-            className="searchbox"
-            onChange={handleInput}
-            onKeyPress={search}
-            />
-        </section>
-    )
+function Search({ search }) {
+  return (
+    <section className="searchbox-wrap">
+      <input
+        type="text"
+        placeholder="Search for a movie..."
+        className="searchbox"
+        onKeyPress={search}
+      />
+    </section>
+  );
 }
 
-export default Search
+export default Search;
