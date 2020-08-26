758bf451a2378ae36ccc12e7c509b70109a5131f
Madhura <madhura.bhujbal@gmail.com>
2020-08-26 15:16:38 +0100

    Remove code for Gif and add code to call component instead

--- day5_moviedb_React/src/components/Results.js
+++ day5_moviedb_React/src/components/Results.js
@@ -6,15 +6,7 @@ function Results({ results, openPopup }) {
   if (results === "not found")
     return (
       <section className="results-not-found">
-        <h3>Sorry! We could not find this movie</h3>
-        <div className="gif">
-          <iframe
-            className="iframe"
-            src="https://gifer.com/embed/v0K"
-            frameBorder="0"
-            allowFullScreen
-          ></iframe>
-        </div>
+        <Gif />
       </section>
     );
   else {
