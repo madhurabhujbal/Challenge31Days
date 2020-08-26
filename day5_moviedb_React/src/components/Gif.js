c0c386b7e6b3628bbd7d017427c5b8d04e0e5f8f
Madhura <madhura.bhujbal@gmail.com>
2020-08-26 15:15:15 +0100

    Add code for rendering Gif in seperate component

--- day5_moviedb_React/src/components/Gif.js
+++ day5_moviedb_React/src/components/Gif.js
@@ -1,7 +1,19 @@
 import React from "react";
 
-function gif() {
-  return <div></div>;
+function Gif() {
+  return (
+    <section className="results-not-found">
+      <h3>Sorry! We could not find this movie</h3>
+      <div className="gif">
+        <iframe
+          className="iframe"
+          src="https://gifer.com/embed/v0K"
+          frameBorder="0"
+          allowFullScreen
+        ></iframe>
+      </div>
+    </section>
+  );
 }
 
-export default gif;
+export default Gif;
