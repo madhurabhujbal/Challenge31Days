a94f9edbefce395d991530c173873a114ed7a4e1
Madhura <madhura.bhujbal@gmail.com>
2020-08-27 17:30:45 +0100

    Rename informationBox to InformationBox

--- day11_weatherAppReact/src/components/informationBox.js
+++ day11_weatherAppReact/src/components/informationBox.js
@@ -1,21 +1,24 @@
 import React from "react";
 
-function informationBox({ weather, dateBuilder }) {
-  return (
-    <div>
-      <div className="location-box">
-        <div className="location">
-          {" "}
-          {weather.name}, {weather.sys.country}{" "}
+function InformationBox({ weather, dateBuilder }) {
+  if (weather.main !== undefined) {
+    return (
+      <div>
+        <div className="location-box">
+          <div className="location">
+            {" "}
+            {weather.name}, {weather.sys.country}{" "}
+          </div>
+          <div className="date">{dateBuilder(new Date())}</div>
         </div>
-        <div className="date">{dateBuilder(new Date())}</div>
+        <div className="weather-box">
+          <div className="temp">{Math.round(weather.main.temp)}°C</div>
+          <div className="weather"> {weather.weather[0].main} </div>
+        </div>
       </div>
-      <div className="weather-box">
-        <div className="temp">{Math.round(weather.main.temp)}°C</div>
-        <div className="weather"> {weather.weather[0].main} </div>
-      </div>
-    </div>
-  );
+    );
+  }
+  return "";
 }
 
-export default informationBox;
+export default InformationBox;
