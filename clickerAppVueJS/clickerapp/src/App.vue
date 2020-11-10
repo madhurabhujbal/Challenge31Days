6d7740c2003f75bf884d8af50fa0d8be2f6029f6
Madhura <madhura.bhujbal@gmail.com>
2020-11-10 13:02:37 +0000

    Import computed from vue and add code to display count*2 using computed

--- clickerAppVueJS/clickerapp/src/App.vue
+++ clickerAppVueJS/clickerapp/src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div> <!-- Will give an error if code is not wrapped in div as vue3 allows only one element in template tag -->
-    <h2> {{ state.count }} </h2>
+    <h2> {{ state.count }}, doubled={{ state.double }} </h2>
     <button @click="updateCount(true)">Increase</button>
     <button @click="updateCount(false)">Decrease</button>
   </div>
 </template>
 
 <script>
-import {reactive} from 'vue';
+import { reactive, computed} from 'vue';
 export default{
   setup() {
     const state = reactive({
-      count: 0
+      count: 0,
+      double: computed(() => state.count * 2)
     });
 
     function updateCount(increase) {
