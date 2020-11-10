2b7803f5d492583f1a07a4179ac5db6f83a1f7f7
Madhura <madhura.bhujbal@gmail.com>
2020-11-10 12:30:55 +0000

    Add function to increment count and wrap html code in div tag

--- clickerAppVueJS/clickerapp/src/App.vue
+++ clickerAppVueJS/clickerapp/src/App.vue
@@ -1,16 +1,23 @@
 <template>
-<h2> {{ count }} </h2>
+  <div> <!-- Will give an error if code is not wrapped in div as vue3 allows only one element in template tag -->
+    <h2> {{ count }} </h2>
+    <button @click="increment">Increase Value</button>
+  </div>
 </template>
 
 <script>
 import {ref} from 'vue';
 export default{
 
-
   setup() {
     const count = ref(0);
+    function increment() {
+      count.value++;
+    }
+
     return {
-      count
+      count,
+      increment
     }
   }
 }
