9861dd0c85f529cac2953241de5b91d867123df7
Madhura <madhura.bhujbal@gmail.com>
2020-11-10 12:45:14 +0000

    Wrap increase and decrease functions into updateCount function

--- clickerAppVueJS/clickerapp/src/App.vue
+++ clickerAppVueJS/clickerapp/src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div> <!-- Will give an error if code is not wrapped in div as vue3 allows only one element in template tag -->
     <h2> {{ count }} </h2>
-    <button @click="increment">Increase</button>
-    <button @click="decrease">Decrease</button>
+    <button @click="updateCount(true)">Increase</button>
+    <button @click="updateCount(false)">Decrease</button>
   </div>
 </template>
 
@@ -12,18 +12,17 @@ export default{
 
   setup() {
     const count = ref(0);
-    function increment() {
+    function updateCount(increase) {
+      if(increase)
       count.value++;
-    }
 
-    function decrease() {
+      else
       count.value--;
     }
 
     return {
       count,
-      increment,
-      decrease
+      updateCount
     }
   }
 }
